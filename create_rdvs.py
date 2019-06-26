aafca89da98492ef73673287738801a1395a8309
John Booth <jbooth04@dcs.bbk.ac.uk>
2019-06-26 13:35:07 +0100

    Started on creating rdvs

---
+++ create_rdvs.py
@@ -0,0 +1,70 @@
+import pyodbc
+import pandas
+import datetime
+import decimal
+import sys
+import time
+import csv
+import os
+
+def create_rdv_complete(cnxn, crsr):
+
+    '''
+
+    :param cnxn: Connection to database
+    :param crsr: Cursor for all SQL operations
+
+    '''
+
+    # get patient attributes
+
+    SQLstring = "SELECT"
+    SQLstring += "       ha_events.event_id, "
+    SQLstring += "       ha_events.patient_id, "
+    SQLstring += "       ha_events.event_type_concept_id, "
+    SQLstring += "       ha_events.start_date, "
+    SQLstring += "       ha_patients.sex, "
+    SQLstring += "       ha_patient_attributes.patient_attribute_id, "
+    SQLstring += "       ha_patient_attributes.patient_attribute_type_concept_id, "
+    SQLstring += "       ha_concepts.value_type_concept_id, "
+    SQLstring += "       ha_patient_attributes.value_numeric, "
+    SQLstring += "       ha_concepts_1.label "
+    SQLstring += "FROM   (((ha_events "
+    SQLstring += "       INNER JOIN ha_patients "
+    SQLstring += "               ON ha_events.patient_id = ha_patients.patient_id) "
+    SQLstring += "       INNER JOIN ha_patient_attributes "
+    SQLstring += "               ON ha_patients.patient_id = ha_patient_attributes.patient_id) "
+    SQLstring += "       INNER JOIN ha_concepts "
+    SQLstring += "               ON ha_patient_attributes.patient_attribute_type_concept_id = ha_concepts.concept_id) "
+    SQLstring += "       LEFT JOIN ha_concepts AS ha_concepts_1 "
+    SQLstring += "               ON ha_patient_attributes.value_concept_id = ha_concepts_1.concept_id"
+    SQLstring += ";"
+
+    crsr.execute(SQLstring)
+    EventPatientAttributeRows = crsr.fetchall()
+
+    for EventPatientAttributeRow in EventPatientAttributeRows:
+        print(EventPatientAttributeRow.event_id)
+
+
+def main():
+
+    rep_conn_str = (
+        r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=I:\DRE\Projects\Research\0004-Post mortem-AccessDB\DataExtraction\PMResearchReportDB.accdb;Uid=Admin;Pwd=;ExtendedAnsiSQL=1;'
+    )
+    rep_cnxn = pyodbc.connect(rep_conn_str)
+    rep_crsr = rep_cnxn.cursor()
+
+    create_rdv_complete(rep_cnxn, rep_crsr)
+
+    destination_folder = "I:\\DRE\\Projects\\Research\\0004-Post mortem-AccessDB\\DataExtraction\\CSVs\\"
+
+    # CreateHASCSVFiles(rep_cnxn, rep_crsr, destination_folder)
+
+    rep_cnxn.close()
+
+
+if __name__ == "__main__":
+    main()
+
