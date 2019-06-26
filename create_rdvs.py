21ccc93bce7bdcc3eeb3a1ec567909ff4f07605b
John Booth <jbooth04@dcs.bbk.ac.uk>
2019-06-26 17:08:40 +0100

    continuing on creating rdvs nearly there with all events and all columns.

--- create_rdvs.py
+++ create_rdvs.py
@@ -178,19 +178,20 @@ def create_rdv_complete(cnxn, crsr):
 
     # Get column Headings
 
-    column_pos = 0
     out_row = []
+    patient_col = []
+    event_col = []
     out_row.append("event_id")
 
     for EventPatientAttributeSummaryRow in EventPatientAttributeSummaryRows:
         out_row.append(get_column_heading(EventPatientAttributeSummaryRow.label))
+        patient_col.append(EventPatientAttributeSummaryRow.patient_attribute_type_concept_id)
 
     for EventAttributeSummaryRow in EventAttributeSummaryRows:
         out_row.append(get_column_heading(EventAttributeSummaryRow.label))
+        event_col.append(EventAttributeSummaryRow.event_attribute_type_concept_id)
 
     writer.writerow(out_row)
-
-    # Rows = crsr.fetchall()
 
     row_counter = 0
     print("")
@@ -203,22 +204,69 @@ def create_rdv_complete(cnxn, crsr):
         sys.stdout.write("\r \r {0}".format(str(row_counter)))
         sys.stdout.flush()
 
-        column_pos = 0
         out_row = []
         out_row.append(EventRow.event_id)
 
-        # for column in crsr.description:
-        #
-        #     if pandas.isnull(Row[column_pos]):
-        #         out_row.append('NULL')
-        #     else:
-        #
-        #         if column[1] == str:
-        #             out_row.append(Row[column_pos])
-        #         else:
-        #             out_row.append(Row[column_pos])
-        #
-        #     column_pos += 1
+        # For each event process list of patient_attributes
+        column_pos = 0
+        for EventPatientAttributeRow in EventPatientAttributeRows:
+            if EventPatientAttributeRow.event_id == EventRow.event_id:
+                # is this the next attribute?
+                if patient_col[column_pos] == EventPatientAttributeRow.patient_attribute_type_concept_id:
+                    out_row.append(EventPatientAttributeRow.patient_attribute_id)
+                else:
+                    out_row.append("NULL")
+                column_pos += 1
+        # may be the last attribute missing
+        if column_pos <= len(patient_col)-1:
+            for col in range(column_pos,len(patient_col)):
+                out_row.append("NULL")
+
+        # For each event process list of event_attributes
+        column_pos = 0
+        for EventAttributeRow in EventAttributeRows:
+            if EventAttributeRow.event_id == EventRow.event_id:
+                # If a row exists for an event it will always be output
+                output_column = True
+                while output_column:
+                    # is this the next attribute?
+                    if event_col[column_pos] == EventAttributeRow.event_attribute_type_concept_id:
+                        # SQLstring += "       ha_event_attributes.value_text, "
+                        # SQLstring += "       ha_event_attributes.value_numeric, "
+                        # SQLstring += "       ha_event_attributes.value_datetime, "
+                        # SQLstring += "       ha_concepts_1.code "
+
+                        if EventAttributeRow.value_type_concept_id == 1:
+                            out_row.append(EventAttributeRow.code)
+                        elif EventAttributeRow.value_type_concept_id == 3:  # INTEGER
+                            out_row.append(EventAttributeRow.value_numeric)
+                        elif EventAttributeRow.value_type_concept_id == 4:  # FLOAT
+                            out_row.append(EventAttributeRow.value_numeric)
+                        elif EventAttributeRow.value_type_concept_id == 5:  # DATE
+                            out_row.append(EventAttributeRow.value_datetime)
+                        elif EventAttributeRow.value_type_concept_id == 6:  # TIME
+                            out_row.append(EventAttributeRow.value_datetime)
+                        elif EventAttributeRow.value_type_concept_id == 7:  # DATETIME
+                            out_row.append(EventAttributeRow.value_datetime)
+                        elif EventAttributeRow.value_type_concept_id == 8:  # TEXT
+                            out_row.append(EventAttributeRow.value_text)
+                        elif EventAttributeRow.value_type_concept_id == 9:  # BOOLEAN
+                            out_row.append(EventAttributeRow.value_numeric)
+                        else:
+                            out_row.append(EventAttributeRow.event_attribute_id)
+                        output_column = False
+                    else:
+                        out_row.append("NULL")
+                    column_pos += 1
+                    # This should be removed
+                    if column_pos >= len(event_col):
+                        break
+            if column_pos >= len(event_col):
+                break
+        # may be the last attributes missing
+        if column_pos <= len(event_col)-1:
+            for col in range(column_pos,len(event_col)):
+                out_row.append("NULL")
 
         writer.writerow(out_row)
 
