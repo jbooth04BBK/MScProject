5be1ef7e1ad804ea14db43f6d3bc26e4634fc81d
John Booth <jbooth04@dcs.bbk.ac.uk>
2019-07-22 23:05:51 +0100

    Stream lining the R modelling

---
+++ RCode/GBoost_study_int1_adj.R
@@ -0,0 +1,160 @@
+# References
+#
+# https://shirinsplayground.netlify.com/2018/11/ml_basics_gbm
+# https://rpubs.com/dalekube/XGBoost-Iris-Classification-Example-in-R
+#
+
+# Clear work space
+rm(list = ls())
+
+# Load libraries
+library(dplyr)
+library(caret)
+library(xgboost)
+
+source("study_functions.R")
+
+set.seed(62)
+
+# Read CSV into R
+# RDVData <- read.csv(file="I:\\DRE\\Projects\\Research\\0004-Post mortem-AccessDB\\DataExtraction\\CSVs\\rdv_study_int1_adj.csv", header=TRUE, sep=",")
+# RDVData <- read.csv(file="I:\\DRE\\Projects\\Research\\0004-Post mortem-AccessDB\\DataExtraction\\CSVs\\rdv_study_int2_adj.csv", header=TRUE, sep=",")
+# RDVData <- read.csv(file="I:\\DRE\\Projects\\Research\\0004-Post mortem-AccessDB\\DataExtraction\\CSVs\\rdv_study_int3_adj.csv", header=TRUE, sep=",")
+RDVData <- read.csv(file="I:\\DRE\\Projects\\Research\\0004-Post mortem-AccessDB\\DataExtraction\\CSVs\\rdv_study_int3_s_adj.csv", header=TRUE, sep=",")
+
+#Remove unwanted columns
+clean_RDVData <- RDVData %>%
+  select(-c(event_id, event_start_date, age_category, case_id, include_in_study, foot_length, crown_rump_length, thymus_weight, thyroid_weight )) %>%
+  na.omit()
+
+clean_RDVData$cod2_summ <- as.factor(clean_RDVData$cod2_summ)
+
+str(clean_RDVData)
+
+# create_train_test(df, size = 0.8, train = TRUE)
+# arguments:
+#  -df: Dataset used to train the model.
+#  -size: Size of the split. By default, 0.8. Numerical value
+#  -train: If set to `TRUE`, the function creates the train set, otherwise the test set. Default value sets to `TRUE`. Boolean value.
+#   You need to add a Boolean parameter because R does not allow to return two data frames simultaneously.
+
+data_train <- create_train_test(clean_RDVData, 0.8, train = TRUE)
+data_test <- create_train_test(clean_RDVData, 0.8, train = FALSE)
+
+#########################################################
+# caret
+
+# Train model with preprocessing & repeated cv
+model_gbm <- caret::train(cod2_summ ~ .,
+                          data = data_train,
+                          method = "gbm",
+                          trControl = trainControl(method = "repeatedcv",
+                                                   number = 5,
+                                                   repeats = 3,
+                                                   verboseIter = FALSE),
+                          verbose = 0)
+model_gbm
+
+#Importance of features
+model_gbm.importance <- summary(model_gbm)
+
+caret::confusionMatrix(
+  data = predict(model_gbm, data_test),
+  reference = data_test$cod2_summ
+)
+
+
+#########################################################
+# XGBoost
+
+xgb.data <- clean_RDVData
+
+num_class = length(levels(xgb.data$cod2_summ))
+cod2_summ = clean_RDVData$cod2_summ
+
+# Convert from class to numeric
+str(xgb.data)
+
+label <- as.integer(xgb.data$cod2_summ) - 1
+xgb.data$cod2_summ = NULL
+
+str(xgb.data)
+
+n = nrow(xgb.data)
+train.index = sample(n,floor(0.80*n))
+train.data = as.matrix(xgb.data[train.index,])
+train.label = label[train.index]
+test.data = as.matrix(xgb.data[-train.index,])
+test.label = label[-train.index]
+
+str(train.data)
+str(train.label)
+
+# Transform the two data sets into xgb.Matrix
+xgb.train = xgb.DMatrix(data=train.data,label=train.label)
+xgb.test = xgb.DMatrix(data=test.data,label=test.label)
+
+# adj 0.001 => 0.30 5=> 9
+params = list(
+  booster="gbtree",
+  eta=0.3,
+  max_depth=9, 
+  gamma=0,
+  subsample=1,
+  colsample_bytree=1,
+  objective="multi:softprob",
+  eval_metric="mlogloss",
+  num_class=num_class
+)
+
+#xgbcv <- xgb.cv( 
+#  params = params, 
+#  data = xgb.train, 
+#  nrounds = 100, 
+#  nfold = 5, 
+#  showsd = T, 
+#  stratified = T, 
+#  print.every_n = 10, 
+#  early.stop_rounds = 20, 
+#  maximize = F)
+
+# Train the XGBoost classifer
+xgb.fit=xgb.train(
+  params=params,
+  data=xgb.train,
+  nrounds=1000,
+  nthreads=1,
+  early_stopping_rounds=10,
+  watchlist=list(val1=xgb.train,val2=xgb.test),
+  verbose=0
+)
+
+# Review the final model and results
+xgb.fit
+
+# Predict outcomes with the test data
+xgb.pred = predict(xgb.fit,test.data,reshape=T)
+xgb.pred = as.data.frame(xgb.pred)
+colnames(xgb.pred) = levels(cod2_summ)
+
+# Use the predicted label with the highest probability
+xgb.pred$prediction = apply(xgb.pred,1,function(x) colnames(xgb.pred)[which.max(x)])
+xgb.pred$label = levels(cod2_summ)[test.label+1]
+
+# Calculate the final accuracy
+result = sum(xgb.pred$prediction==xgb.pred$label)/nrow(xgb.pred)
+print(paste("Final Accuracy =",sprintf("%1.2f%%", 100*result)))
+
+# Create confusion matrix
+table_mat <- table(xgb.pred$label, xgb.pred$prediction)
+table_mat
+
+accuracy_Test <- sum(diag(table_mat)) / sum(table_mat)
+
+print(paste('Accuracy for test', accuracy_Test))
+
+importance <- xgb.importance(model = xgb.fit)
+importance
+
+xgb.plot.importance (importance_matrix = importance[1:30]) 
+ 
